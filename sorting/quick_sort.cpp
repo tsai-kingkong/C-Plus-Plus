7835e9581e403526be846825b3b7d7b255935b77
kingkong <pa015596@gmail.com>
2021-01-27 14:06:06 +0800

    qucik_sort

--- sorting/quick_sort.cpp
+++ sorting/quick_sort.cpp
@@ -24,6 +24,17 @@
 #include <cstdlib>
 #include <iostream>
 
+
+// prints the array after sorting
+void show(int arr[], int size) {
+    for (int i = 0; i < size; i++) std::cout << arr[i] << " ";
+    std::cout << "\n";
+}
+void show(int arr[], int low, int high) {
+    for (; low<=high ; low++) std::cout<< arr[low] << " ";
+    std::cout << "\n";
+}
+
 namespace sorting {
 /**
  *      This function takes last element as pivot, places
@@ -34,7 +45,7 @@ namespace sorting {
  *
  */
 
-int partition(int arr[], int low, int high) {
+int partition2(int arr[], int low, int high) {
     int pivot = arr[high];  // taking the last element as pivot
     int i = (low - 1);      // Index of smaller element
 
@@ -54,6 +65,57 @@ int partition(int arr[], int low, int high) {
     return (i + 1);
 }
 
+int partition3(int arr[], int l, int r){
+    int pivot = arr[l];
+    int i = l;
+    for(int j=l+1; j<=r;j++){
+        if(arr[j] < pivot){
+            int temp = arr[j];
+            arr[j] = arr[++i];
+            arr[i] = temp;
+        }
+    }
+    arr[l] = arr[i];
+    arr[i] = pivot; 
+    return i;
+}
+
+int partition_(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low-1;
+    for(int j=low; j<high; j++){
+        if(arr[j]>pivot){
+            i++;
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    arr[high] = arr[++i];
+    arr[i] = pivot;
+    return i;
+    
+
+}
+
+int partition(int arr[], int low, int high){
+    printf("low=%d, high=%d\n", low, high);
+    int pivot = arr[high];
+    int i = low, j = high-1;
+    while(i<=j){ //=的情況發生在只有兩個元素, ex:3 2
+        while(i < high && arr[i] <= pivot) i++;
+        while(j >= low && arr[j] > pivot) j--; //j>=low , arr[j] <= pivot
+        if(i<j){
+            printf("swap %d,%d\n",i,j);
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    printf("j=%d\n",j);
+    std::swap(arr[j+1], arr[high]);
+    return j+1;
+}
+
+
 /**
  *      The main function that implements QuickSort
  *      arr[] --> Array to be sorted,
@@ -63,20 +125,40 @@ int partition(int arr[], int low, int high) {
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int p = partition(arr, low, high);
+        show(arr, low, high);
+        printf("partition done\n");
         quickSort(arr, low, p - 1);
         quickSort(arr, p + 1, high);
     }
 }
 
-}  // namespace sorting
+void quickSort2(int arr[], int low, int high){
+    std::cout<<"quickSort2"<<std::endl;
+    show(arr, low, high);
+    if(low >= high)
+        return ;
+    int pivot = arr[low];
+    int i = low+1, j = high;
+    while(i <= j){
+        while(i<high && arr[i] <= pivot) i++;
+        while(j>low && arr[j] > pivot) j--;
+        if (i < j){
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    if(j != low){
+        std::swap(arr[low], arr[j]);
+    }
+    show(arr, low, high);
+    quickSort2(arr, low, j-1);
+    quickSort2(arr, j+1, high);
 
-using sorting::quickSort;
-
-// prints the array after sorting
-void show(int arr[], int size) {
-    for (int i = 0; i < size; i++) std::cout << arr[i] << " ";
-    std::cout << "\n";
 }
+
+}  // namespace sorting
+using sorting::quickSort;
+using sorting::quickSort2;
+
 
 /** Driver program to test above functions */
 int main() {
@@ -93,7 +175,9 @@ int main() {
         std::cout << "\n";
         std::cin >> arr[i];
     }
-    quickSort(arr, 0, size);
+    std::cout << "Sorted array\n";
+    show(arr, size);
+    quickSort(arr, 0, size-1);
     std::cout << "Sorted array\n";
     show(arr, size);
     delete[] arr;
